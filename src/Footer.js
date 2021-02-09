a79aca263c6ff5c164e4cc3014fe3bdc30c6e10e
tawong1 <tawong1@cs.washington.edu>
2021-02-08 21:32:36 -0800

    Changed footer and got social media icons

--- src/Footer.js
+++ src/Footer.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import './App.css';
+import facebookicon from "./images/facebookicon.png";
+import instagramicon from "./images/instagramicon.png";
+import twittericon from "./images/twittericon.png";
 
 function Footer() {
   return (
     <footer>
       <div>
-        <h3>Contact</h3>
         <ul>
           <li>770-767-0200</li>
+          <li>info@platformofhope.org</li>
           <li>P.O. Box 48198</li>
         </ul>
       </div>
       <div>
-        <h3>Social Media</h3>
-        <ul>
-          <li>Facebook</li>
-          <li>Twitter</li>
-          <li>LinkedIn</li>
-        </ul>
-      </div>
-      <div>
-        <h3>Other Info</h3>
-        <ul>
-          <li>Resource 1</li>
-          <li>Resource 2</li>
-          <li>Resource 3</li>
-        </ul>
+        <div className="social-media-icons">
+          <a href="https://www.facebook.com/platformofhope1" target="_blank" rel="noopener noreferrer">
+            <img src={facebookicon} alt="Facebook" className="facebook"/>
+          </a>
+          <a href="https://twitter.com/login" target="_blank" rel="noopener noreferrer">
+            <img src={twittericon} alt="Twitter" className="icon"/>
+          </a>
+          <a href="https://www.instagram.com/platform_of_hope/" target="_blank" rel="noopener noreferrer">
+            <img src={instagramicon} alt="Instagram" className="instagram"/>
+          </a>
+        </div>
       </div>
     </footer>
   );

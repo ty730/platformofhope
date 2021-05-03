b2d27ba83f7c6bd4c0a19cda6da458bcd90d5d24
tawong1 <tawong1@cs.washington.edu>
2021-05-03 16:06:39 -0700

    changed calendar styles

--- README.md
+++ README.md
@@ -6,21 +6,19 @@
 ### News & events:
 - ~~Add calendar~~
 - ~~Make slideshow (swiper js)~~
-- Create new slideshow for videos (centered auto)
-- Draft on Figma (Christian)
-- Make news and event information a seperate component
+- ~~Draft on Figma (Christian)~~
+- ~~Make news and event information a seperate component~~
 ### Homepage
 - ~~Nav bar and footer~~ (Christian)
 - ~~Icon images with text below them~~ (Christian)
-- Draft from Figma to website (Tyler)
-- Featured events (Christian)
-- Swap poster to left side and make smaller/Draft on Figma (Tyler)
+- ~~Draft from Figma to website (Tyler)~~
+- ~~Featured events (Christian)~~
 ### About
-- Draft on Figma
+- ~~Draft on Figma~~
 ### Services
 - ~~Create layout from Figma (try bootstrap)~~
 ### Contact (Save until later)
-- Draft on Figma
+- ~~Draft on Figma~~
 ### Donate
 - ~~Draft onto website~~
 - ~~Make Appropriation of funding responsive~~
--- src/Pages/NewsAndEvents.js
+++ src/Pages/NewsAndEvents.js
@@ -3,12 +3,8 @@ import './../App.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment';
-import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation, Pagination } from 'swiper';
 import 'swiper/swiper-bundle.css';
-import img1 from './../images/image-1.jpg';
-import img2 from './../images/image-2.jpg';
-import thumb from './../images/ig-vid-thumb.JPG';
 
 const localizer = momentLocalizer(moment);
 
@@ -32,69 +28,34 @@ function NewsAndEvents() {
       title: 'All Day Event',
     },
   ];
-  const imageSrcs = [img1, img2, img1, img2, img1, img2]
-  const slides = [];
-  for (let i = 0; i < imageSrcs.length; i++) {
-    slides.push(
-      <SwiperSlide key={`slide-${i}`} tag="li">
-        <img className="slider-img" src={imageSrcs[i]} alt=""/>
-      </SwiperSlide>
-    )
-  }
-  const videoSrcs = [thumb, thumb, thumb]
-  const videoSlides = [];
-  for (let j = 0; j < videoSrcs.length; j++) {
-    videoSlides.push(
-      <SwiperSlide key={`slide-${j}`} tag="li">
-        <a href="https://www.instagram.com/p/CD6sXzWHLZX/?utm_source=ig_web_copy_link"
-           target="_blank" rel="noopener noreferrer">
-          <img className="slider-img" src={videoSrcs[j]} alt=""/>
-        </a>
-      </SwiperSlide>
-    )
-  }
   return (
     <div>
       <div className="home-image">
         <h1>News & Events</h1>
       </div>
       <div className="calendar-container">
-      <Calendar
-        localizer={localizer}
-        events={dummyEvents}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-      />
+        <Calendar
+          localizer={localizer}
+          events={dummyEvents}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 700 }}
+          eventPropGetter={
+            (event, start, end, isSelected) => {
+              let newStyle = {
+                backgroundColor: "#faa919",
+                color: 'white',
+                borderRadius: "0px",
+                border: "none"
+              };
+              return {
+                className: "",
+                style: newStyle
+              };
+            }
+          }
+        />
       </div>
-      <div className="image-slides">
-        <Swiper
-          tag="section"
-          wrapperTag="ul"
-          navigation
-          pagination
-          spaceBetween={0}
-          slidesPerView={1}
-        >
-          {slides}
-          
-        </Swiper>
-      </div>
-      <div className="video-slides">
-        <Swiper
-          tag="section"
-          wrapperTag="ul"
-          navigation
-          pagination
-          centeredSlides
-          spaceBetween={0}
-          slidesPerView={"auto"}
-        >
-          {videoSlides}
-        </Swiper>
-      </div>
-
-      
     </div>
   );
 }

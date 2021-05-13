4d57ea43911d883dcb79c295ac735d88b07c3680
tawong1 <tawong1@cs.washington.edu>
2021-05-13 13:35:36 -0700

    Changed news and events

--- src/Components/Events/index.js
+++ src/Components/Events/index.js
@@ -1,9 +1,15 @@
 export const Events =
-{
-
-
-		title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, praesentium?',
-		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quaerat ipsum et repudiandae sapiente veritatis, optio voluptatem, iusto adipisci, non magnam facilis. Temporibus, mollitia accusamus.',
-		date: '04/12/2021',
-
-}
+[
+	{
+		allDay: false,
+		end: new Date('March 10, 2021 11:13:00'),
+		start: new Date('March 09, 2021 11:13:00'),
+		title: 'hi',
+	},
+	{
+		allDay: true,
+		end: new Date('March 09, 2021 11:13:00'),
+		start: new Date('March 09, 2021 11:13:00'),
+		title: 'All Day Event',
+	},
+];
--- src/Pages/GetHelp.js
+++ src/Pages/GetHelp.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './../App.css';
+import emailjs from 'emailjs-com';
 import servicesImg from './../images/get-help-services-edited.png';
 import resourceGuideImg from './../images/resource-guide.png';
 
@@ -7,6 +8,19 @@ import resourceGuideImg from './../images/resource-guide.png';
  * This is the Get Help component that holds all information for the Get Help page.
  */
 function GetHelp() {
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_8fderar', 'template_6hjo9b6', e.target, 'user_OKR16ehhZhBLdFc7CF3bH')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    e.target.reset();
+  }
+
   return (
     <div className="GetHelp">
       <div className="get-help-top">
@@ -26,6 +40,55 @@ function GetHelp() {
           <p>Find other resouces in the Atlanta Area that may be able to help</p>
           <button>DOWNLOAD</button>
         </div>
+      </div>
+      <div className="outreach-container">
+        <h2>Outreach Request Form</h2>
+        <p>Please fill out this outreach form and we will be in touch with you shortly.</p>
+        <form className="outreach-form" onSubmit={sendEmail}>
+          <input type="text" placeholder="Name" name="name" required/>
+          <input type="email" placeholder="Email Address" name="email" required/>
+          <input type="text" placeholder="Phone Number" name="phone" required/>
+          <input type="text" placeholder="Address" name="address"/>
+          <select name="numpeople" required>
+            <option value="" selected disabled hidden>Number of people in household</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+          <input type="number" placeholder="Household income" name="income" min="0" required/>
+          <p>Which benefits do you recieve?</p>
+          <div>
+            <input type="checkbox" name="foodstamps"/>
+            <label for="foodstamps">SNAP/Food Stamps</label>
+          </div>
+          <div>
+            <input type="checkbox" name="medicaid"/>
+            <label for="medicaid">Medicaid</label>
+          </div>
+          <div>
+            <input type="checkbox" name="tanf"/>
+            <label for="tanf">TANF</label>
+          </div>
+          <div>
+            <input type="checkbox" name="disability"/>
+            <label for="disability">Disability</label>
+          </div>
+          <textarea placeholder="Grade(s) and School(s) of child(ren)" name="childschool"/>
+          <input type="text" placeholder="How were you referred?" name="referal"/>
+          <select name="veteran" required>
+            <option value="" selected disabled hidden>Are you a veteran?</option>
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+          </select>
+          <input className="outreach-submit" type="submit" value="SUBMIT" />
+        </form>
       </div>
     </div>
   );

09888545d279b108575c9a5067ccbc5968df7730
tawong1 <tawong1@cs.washington.edu>
2021-05-16 15:26:12 -0700

    Finished styling on Get Help

--- src/Pages/GetHelp.js
+++ src/Pages/GetHelp.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './../App.css';
 import emailjs from 'emailjs-com';
+import Select from 'react-select';
 import servicesImg from './../images/get-help-services-edited.png';
 import resourceGuideImg from './../images/resource-guide.png';
 
@@ -19,6 +20,40 @@ function GetHelp() {
           console.log(error.text);
       });
     e.target.reset();
+  }
+
+  const numbersOptions = [
+    { value: '1', label: '1' },
+    { value: '2', label: '2' },
+    { value: '3', label: '3' },
+    { value: '4', label: '4' },
+    { value: '5', label: '5' },
+    { value: '6', label: '6' },
+    { value: '7', label: '7' },
+    { value: '8', label: '8' },
+    { value: '9', label: '9' },
+    { value: '10', label: '10' },
+  ];
+
+  const yesNoOptions = [
+    { value: 'yes', label: 'yes' },
+    { value: 'no', label: 'no' },
+  ];
+
+  const selectStyles = {
+    placeholder: (defaultStyles) => ({
+      ...defaultStyles,
+      color: 'lightgray',
+      letterSpacing: '2px',
+    }),
+    control: (provided, state) => ({
+      ...provided,
+      paddingTop: 13,
+      paddingBottom: 13,
+      paddingLeft: 5,
+      marginTop: '4%',
+      border: 'solid 1px gray',
+    })
   }
 
   return (
@@ -49,44 +84,36 @@ function GetHelp() {
           <input type="email" placeholder="Email Address" name="email" required/>
           <input type="text" placeholder="Phone Number" name="phone" required/>
           <input type="text" placeholder="Address" name="address"/>
-          <select name="numpeople" required>
-            <option value="" selected disabled hidden>Number of people in household</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
+          <Select
+            options={numbersOptions}
+            placeholder={'Number of people in household'}
+            styles={selectStyles}
+          />
           <input type="number" placeholder="Household income" name="income" min="0" required/>
           <p>Which benefits do you recieve?</p>
-          <div>
+          <div className="checkbox">
             <input type="checkbox" name="foodstamps"/>
             <label for="foodstamps">SNAP/Food Stamps</label>
           </div>
-          <div>
+          <div className="checkbox">
             <input type="checkbox" name="medicaid"/>
             <label for="medicaid">Medicaid</label>
           </div>
-          <div>
+          <div className="checkbox">
             <input type="checkbox" name="tanf"/>
             <label for="tanf">TANF</label>
           </div>
-          <div>
+          <div className="checkbox">
             <input type="checkbox" name="disability"/>
             <label for="disability">Disability</label>
           </div>
           <textarea placeholder="Grade(s) and School(s) of child(ren)" name="childschool"/>
           <input type="text" placeholder="How were you referred?" name="referal"/>
-          <select name="veteran" required>
-            <option value="" selected disabled hidden>Are you a veteran?</option>
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
-          </select>
+          <Select
+            options={yesNoOptions}
+            placeholder={'Are you a veteran?'}
+            styles={selectStyles}
+          />
           <input className="outreach-submit" type="submit" value="SUBMIT" />
         </form>
       </div>

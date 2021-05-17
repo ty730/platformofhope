053f3b6b778937f60891bd42904f121c6cbf961d
tawong1 <tawong1@cs.washington.edu>
2021-05-17 12:24:31 -0700

    Started calendar popups

--- src/Pages/NewsAndEvents.js
+++ src/Pages/NewsAndEvents.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './../App.css';
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment';
@@ -15,27 +18,34 @@ SwiperCore.use([Navigation, Pagination]);
  * This is the Home component that holds all information for the Home page.
  */
 function NewsAndEvents() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   const dummyEvents = [
     {
       allDay: false,
-      end: new Date('March 10, 2021 11:13:00'),
-      start: new Date('March 09, 2021 11:13:00'),
-      title: 'hi',
+      end: new Date('May 10, 2021 11:13:00'),
+      start: new Date('May 09, 2021 11:13:00'),
+      title: 'Test Event',
     },
     {
       allDay: true,
-      end: new Date('March 09, 2021 11:13:00'),
-      start: new Date('March 09, 2021 11:13:00'),
+      end: new Date('May 09, 2021 11:13:00'),
+      start: new Date('May 09, 2021 11:13:00'),
       title: 'All Day Event',
     },
   ];
   return (
     <div>
-      <div className="home-image">
+      <div className="news-events-image">
         <h1>News & Events</h1>
       </div>
       <div className="calendar-container">
         <Calendar
+          selectable={true}
+          onSelectEvent={handleShow}
           localizer={localizer}
           events={dummyEvents}
           startAccessor="start"
@@ -56,6 +66,17 @@ function NewsAndEvents() {
             }
           }
         />
+        <Modal show = {show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Test Title</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>This is the description</Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     </div>
   );

3a88cb91082ef869cc6b819f428bcba9f1f8ed4d
tawong1 <tawong1@cs.washington.edu>
2021-05-31 18:23:47 -0700

    Continued working on Media

--- src/Pages/NewsAndEvents.js
+++ src/Pages/NewsAndEvents.js
@@ -124,9 +124,6 @@ function NewsAndEvents() {
           watchSlidesVisibility
           watchSlidesProgress
           slidesPerView={4}
-          freeMode={true}
-          slideToClickedSlide
-          centeredSlides
         >
           {slides}
         </Swiper>
